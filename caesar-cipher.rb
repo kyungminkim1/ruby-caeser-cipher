5291abca81020d427d9e16b153588d50c90fbdbe
kyungminkim1 <kyungminkimnz@gmail.com>
2023-01-07 17:21:08 +1300

    Update pseudocode

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,14 +1,15 @@
 # caesar_cipher() accepts two arguments, a string and shift_factor (int)
-    # split the string into individual characters
-    # for each char
-    # check if char is a letter
-        # get the ascii value
-        # apply the shift_factor
+
+    # encode string to US-ASCII
+    # convert string to bytes array
+    # for each ascii value
+    # check if ascii value converts to a letter
         # check if new ascii value is greater than 90 (uppercase z)
-            # subtract that ascii value by 90
-            # add the result by 64 (ascii value before uppercase a)
+            # add shift_factor to ascii value and apply modulo
+            # add the result by 65 (ascii value before uppercase a)
         # else check if new ascii value if greater than 122 (lowercase z)
-            # add the result by 96 (ascii value before lowercase a)
+            # add shift_factor to ascii value and apply modulo
+            # add the result by 97 (ascii value before lowercase a)
         # convert new ascii result to char
     
     # concat char together
@@ -18,13 +19,13 @@ def caesar_cipher(string, shift_factor)
     chars = string.split("")
     new_chars = chars.map do |char|
         ascii_value = char.ord
-        # check if uppercase letter
+        # check if uppercase letter        
         if ascii_value >= 65 && ascii_value <= 90
-            ascii_value = 65 + (ascii_value + shift_factor) % 90
+            ascii_value = 65 + (ascii_value + shift_factor) % 65
         elsif ascii_value >= 97 && ascii_value <= 122
-            ascii_value = 97 + (ascii_value + shift_factor) % 122
-        new_char = ascii_value.chr
+            ascii_value = 97 + (ascii_value + shift_factor) % 97
         end
+        new_char = ascii_value.chr
     end
     
     new_string = new_chars.join

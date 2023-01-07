9fc9f428d8b0c3d6623d1742bbb199e8bb59b51c
kyungminkim1 <kyungminkimnz@gmail.com>
2023-01-07 16:28:47 +1300

    Add caeser_cipher pseudocode

--- caeser-cipher.rb
+++ caeser-cipher.rb
@@ -1,3 +1,19 @@
+# caeser_cipher() accepts two arguments, a string and shift_factor (int)
+    # split the string into individual characters
+    # for each char
+    # check if char is a letter
+        # get the ascii value
+        # apply the shift_factor
+        # check if new ascii value is greater than 90 (uppercase z)
+            # subtract that ascii value by 90
+            # add the result by 64 (ascii value before uppercase a)
+        # else check if new ascii value if greater than 122 (lowercase z)
+            # add the result by 96 (ascii value before lowercase a)
+        # convert new ascii result to char
+    
+    # concat char together
+
+
 def caeser_cipher()
 
 end

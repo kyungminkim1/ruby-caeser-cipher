36638a527c03e120160846debf709b3fc309d4cb
kyungminkim1 <kyungminkimnz@gmail.com>
2023-01-07 17:50:25 +1300

    Complete caesar_cipher function

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -5,25 +5,37 @@
     # for each ascii value
     # check if ascii value converts to a letter
         # check if new ascii value is greater than 90 (uppercase z)
-            # add shift_factor to ascii value and apply modulo
-            # add the result by 65 (ascii value before uppercase a)
+            # add shift_factor to ascii value and apply modulo 90
+            # add the result by 64 (ascii value before uppercase a)
         # else check if new ascii value if greater than 122 (lowercase z)
             # add shift_factor to ascii value and apply modulo
-            # add the result by 97 (ascii value before lowercase a)
+            # add the result by 96 (ascii value before lowercase a)
         # convert new ascii result to char
     
     # concat char together
 
 
 def caesar_cipher(string, shift_factor)
-    chars = string.split("")
-    new_chars = chars.map do |char|
-        ascii_value = char.ord
+    string = string.encode("US-ASCII")
+    ascii_values = string.bytes
+    new_chars = ascii_values.map do |ascii_value|
         # check if uppercase letter        
         if ascii_value >= 65 && ascii_value <= 90
-            ascii_value = 65 + (ascii_value + shift_factor) % 65
+            ascii_value = ascii_value + shift_factor
+            # check if ascii_value exceeds range
+            if ascii_value > 90
+                ascii_value = 64 + (ascii_value % 90)
+            elsif ascii_value < 65
+                ascii_value = 90 + (ascii_value - 64)
+            end
         elsif ascii_value >= 97 && ascii_value <= 122
-            ascii_value = 97 + (ascii_value + shift_factor) % 97
+            ascii_value = ascii_value + shift_factor
+            # check if ascii_value exceeds range
+            if ascii_value > 122
+                ascii_value = 96 + (ascii_value % 122)
+            elsif ascii_value < 97
+                ascii_value = 122 + (ascii_value - 96)
+            end
         end
         new_char = ascii_value.chr
     end
@@ -31,4 +43,4 @@ def caesar_cipher(string, shift_factor)
     new_string = new_chars.join
 end
 
-puts caesar_cipher("What a string!", 5)
+puts caesar_cipher("What a string!", 25)
